bfdd1d355e6ed80a9ae36a2e12893a7a24391a21
Subhpreet Singh <subhpreetsingh657@gmail.com>
2025-09-21 08:46:35 -0500

    feat: Done with the work on the header of the campus organizer website.

--- Campus_Event_Organizer_RRC/src/App.tsx
+++ Campus_Event_Organizer_RRC/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./Components/header/header";
 import EventCategories from "./Components/event_category/event_category";
  
 function App() {
   return (
    
     <>
+      <Header />
       < EventCategories />
     </>
   );
