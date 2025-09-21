47cc92ef110b8022a5ceb45521b1836a2ffc36d5
Subhpreet Singh <subhpreetsingh657@gmail.com>
2025-09-21 15:30:13 -0500

    feat: Adding a footer with proper team collaboration and team member names.

--- Campus_Event_Organizer_RRC/src/App.tsx
+++ Campus_Event_Organizer_RRC/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./Components/header/header";
+import Footer from "./Components/footer/footer";
 import EventCategories from "./Components/event_category/event_category";
  
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       < EventCategories />
+      <Footer />
     </>
   );
 };
