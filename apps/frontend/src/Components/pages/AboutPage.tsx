b97c1c832dfb0be2cc1ad30d780b195b14fed056
Lovedeep Singh Sidhu <sidhulovedeep8119.com>
2025-11-15 17:31:17 -0600

    i updated useEventState to correctly use service and sync repository data and also split up my eventlist form to properly use it own hook and also in ap.tsx file moved individual components to seperate files in pages . fixed almost each of the problems and issues addressed bu instructer

---
+++ apps/frontend/src/Components/pages/AboutPage.tsx
@@ -0,0 +1,18 @@
+import Header from "../header/header";
+
+function AboutPage() {
+  return (
+    <>
+      <Header />
+      <main className="p-4">
+        <h2>About Campus Event Organizer</h2>
+        <p>
+          This platform helps students explore, organize, and participate in
+          campus events to enhance community engagement.
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default AboutPage;
